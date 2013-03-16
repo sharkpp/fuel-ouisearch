c3bf81a357ff8de2f5b697e3f3520e8fb3fa0e45
sharkpp <webmaster@sharkpp.net>
2013-03-16 13:13:52 +0000

    Travis CIでのテスト対象を追加。 Travis CI用の設定ファイルを直接生成するのではなくあらかじめ用意したファイルをコピーするように変更。
    git-svn-id: http://nasserver:888/repos/main/platform/fuelphp/fuel-ouisearch@2410 fd21481f-4f5f-b64a-a7cc-914020ce4b36

---
+++ tests/travis-ci/config.php
@@ -0,0 +1,25 @@
+<?php
+	return array(
+		'security' => array(
+			'uri_filter' => array(
+				'htmlentities'
+			),
+			'output_filter' => array(
+				'Security::htmlentities'
+			),
+			'whitelisted_classes' => array(
+				'Fuel\\Core\\Response',
+				'Fuel\\Core\\View',
+				'Fuel\\Core\\ViewModel',
+				'Closure'
+			),
+		),
+		'package_paths' => array(
+			PKGPATH
+		),
+		'always_load' => array(
+			'packages' => array(
+				'ouisearch'
+			)
+		)
+	);
